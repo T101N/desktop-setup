95a464c5daabe1cc96a731054aaf8fc71076534c
tony <no-email>
2018-01-20 23:05:47 +1000

    Added script to install nodejs

---
+++ fedora/nodejs/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#####  Install nodejs  #####
+
+# Install guide - https://nodejs.org/en/download/package-manager/#enterprise-linux-and-fedora
+NODE_VERSION=8.x
+curl --silent --location https://rpm.nodesource.com/setup_${NODE_VERSION} \
+    | sudo bash - \
+    && sudo dnf -y install nodejs \
+    && sudo dnf install gcc-c++ make 
+
+# Fixing permissions - https://docs.npmjs.com/getting-started/fixing-npm-permissions
+mkdir -p ~/.npm-global \
+    && npm config set prefix '~/.npm-global' \
+    && echo "export PATH=\$PATH:~/.npm-global/bin" >> ~/.profile
+    # && echo "source ~/.profile" >> ~/.bashrc 
+
+source ~/.profile
