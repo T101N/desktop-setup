b087765eabd061ebe1acc523ae1c9686ecbfab28
Tony N <4016013+T101QUT@users.noreply.github.com>
2018-02-12 17:40:25 +1000

    allow users to use docker-compose

--- fedora/docker-ce/install.sh
+++ fedora/docker-ce/install.sh
@@ -30,3 +30,5 @@ sudo usermod -a `whoami` -G docker
 # Install docker-compose
 #https://docs.docker.com/compose/install/#install-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
