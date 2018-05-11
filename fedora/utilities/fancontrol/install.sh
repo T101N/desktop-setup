d64ac1e465001bb9f8dacbbefbac2c57ce3132e2
Tony N <4016013+T101QUT@users.noreply.github.com>
2018-05-11 22:59:14 +1000

    added script to rerun fancontrol on wakup. updated install script

--- fedora/utilities/fancontrol/install.sh
+++ fedora/utilities/fancontrol/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 DIR=$(dirname ${BASH_SOURCE[0]})
+RESTART_ON_RESUME_FILE=90-fancontrol.sleep
 
 sudo dnf install lm_sensors
-cp ${DIR}/fancontrol /etc/fancontrol
+sudo cp ${DIR}/fancontrol /etc/fancontrol
+
+# sudo cp 90-fancontrol.sleep /lib/systemd/system-sleep/90-fancontrol.sleep
+sudo cp ${DIR}/${RESTART_ON_RESUME_FILE} /lib/systemd/system-sleep/${RESTART_ON_RESUME_FILE}
+
+sudo systemctl enable fancontrol
+sudo systemctl start fancontrol
