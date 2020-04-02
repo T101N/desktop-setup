8668ae0d9a851dddf261bff5988f9bfe46b16a6e
Tony N <900692-t101@users.noreply.gitlab.com>
2020-04-02 11:05:13 +1000

    Change to show list instead of script

---
+++ fedora/extensions.md
@@ -0,0 +1,9 @@
+# Extension list
+
+Dash to dock
+Freon
+No Topleft Hot Corner
+Drive Menu
+Frippery Move Clock
+Hide Activities Button
+Sound Input & Output Device Chooser
--- fedora/extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo dnf install -y \
-  gnome-shell-extension-dash-to-dock \
-  gnome-shell-extension-freon \
-  gnome-shell-extension-no-topleft-hot-corner \
-  gnome-shell-extension-drive-menu
-  
