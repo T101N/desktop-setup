e76f6efe4c096c07d93ef35f8438950d5c76a576
Tony Nguyen <900692-t101@users.noreply.gitlab.com>
2022-07-03 22:21:19 +1000

    update to use gradle 7.4

--- fedora/java/install.sh
+++ fedora/java/install.sh
@@ -4,4 +4,4 @@ sudo dnf install -y java-latest-openjdk-devel
 
 curl -s "https://get.sdkman.io" | bash \
     && source "$HOME/.sdkman/bin/sdkman-init.sh" \
-    && sdk install gradle 6.3
+    && sdk install gradle 7.4
