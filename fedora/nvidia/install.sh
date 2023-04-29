69cb4aa723320d79a8f464295e33f9bc7a4c20a3
Tony Nguyen <900692-t101@users.noreply.gitlab.com>
2023-04-29 21:20:13 +1000

    added service to lower gpu power on startup

--- fedora/nvidia/install.sh
+++ fedora/nvidia/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) 2>&1 > /dev/null && pwd)
+LOWER_GPU_POWER_SERVICE=lower-gpu-power.service
+
 sudo dnf install -y \
     xorg-x11-drv-nvidia \
     xorg-x11-drv-nvidia-cuda \
@@ -15,3 +18,7 @@ sudo dnf install -y \
     vdpauinfo \
     libva-vdpau-driver \
     libva-utils
+
+sudo cp -f ${DIR}/${LOWER_GPU_POWER_SERVICE} /etc/systemd/system/${LOWER_GPU_POWER_SERVICE} \
+    && sudo systemctl enable ${LOWER_GPU_POWER_SERVICE} \
+    && sudo systemctl start ${LOWER_GPU_POWER_SERVICE}
