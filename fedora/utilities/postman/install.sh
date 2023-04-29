82af10e08d032e00dcbfcc4782f2c0fffb0fab31
Tony Nguyen <900692-t101@users.noreply.gitlab.com>
2023-04-29 21:19:43 +1000

    updated postman install script

--- fedora/utilities/postman/install.sh
+++ fedora/utilities/postman/install.sh
@@ -9,10 +9,12 @@ declare EXTRACTED_DIR=${EXTRACTED_DIR:-"Postman"}
 declare INSTALL_DIR=${INSTALL_DIR:-"/opt/postman"}
 declare WORKDIR=${WORKDIR:-"/tmp"}
 
-# mkdir -p ${INSTALL_DIR} \
-#     && curl -o ${WORKDIR}/${ARCHIVE_NAME} https://dl.pstmn.io/download/latest/linux64 \
-#     && tar -xzf ${WORKDIR}/${ARCHIVE_NAME} \
-#     && sudo mv -f ${WORKDIR}/${EXTRACTED_DIR} ${INSTALL_DIR} \
-    cp ${DIR}/postman.desktop ${INSTALL_DIR}/postman.desktop \
-    && ln -s ${INSTALL_DIR}/postman.desktop ~/.local/share/applications/postman.desktop
-    
+declare USER=${USER}
+
+#     && curl -o ${WORKDIR}/${ARCHIVE_NAME} https://dl.pstmn.io/download/latest/linux_64 \
+
+# sudo mkdir -p ${INSTALL_DIR} \
+#     && sudo chown ${USER}:${USER} ${INSTALL_DIR} \
+    tar -xzf ${WORKDIR}/${ARCHIVE_NAME} -C ${INSTALL_DIR} \
+    && cp -f ${DIR}/postman.desktop ${INSTALL_DIR}/postman.desktop \
+    && ln -s ${INSTALL_DIR}/postman.desktop ~/.local/share/applications/postman.desktop \
